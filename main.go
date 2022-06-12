23a8ab87701fd607dcf9f8de6976f9a0b202e818
kyunghyunHan <ekfqlwcjswl@gmail.com>
2022-06-12 17:13:09 +0900

    add

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
 
 var (
-// router = gin.Default()
+	router = gin.Default()
 )
 
 func main() {
-	fmt.Println("jwt")
+	router.POST("/login", Login)
+	log.Fatal(router.Run(":8080"))
 }
